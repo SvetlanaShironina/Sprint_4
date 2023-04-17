c9df16fffc7aaf2a5492844370ce09a89f0cf3db
Sveta <Shironin10@gmail.com>
2023-04-17 20:32:14 +0300

    edited test logo Yandex

--- src/test/java/logo/LogoYandexSuccessfulTransitionTest.java
+++ src/test/java/logo/LogoYandexSuccessfulTransitionTest.java
@@ -4,13 +4,11 @@ import baseTest.CommonBaseTest;
 import model.MainPage;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertEquals;
 
 public class LogoYandexSuccessfulTransitionTest extends CommonBaseTest{
 
